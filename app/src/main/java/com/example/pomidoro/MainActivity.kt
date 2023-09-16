ee5e1a72dc6da9fa4fde7ca9a234c29f455de813
oliwia <oliwia.giercza@gmail.com>
2023-09-16 20:42:50 +0100

    the timer stops when reached 00:00

--- app/src/main/java/com/example/pomidoro/MainActivity.kt
+++ app/src/main/java/com/example/pomidoro/MainActivity.kt
@@ -14,6 +14,7 @@ import java.util.TimerTask
 class MainActivity : AppCompatActivity() {
     private lateinit var buttonStart: Button
     private lateinit var viewTimer: Chronometer
+    private var countdownTimer: CountDownTimer? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +23,20 @@ class MainActivity : AppCompatActivity() {
         buttonStart = findViewById(R.id.button_start)
         viewTimer = findViewById(R.id.view_timer)
         viewTimer.isCountDown = true
-        buttonStart.setOnClickListener{
+
+        countdownTimer = object : CountDownTimer(20000, 1000) { // 20000 milliseconds = 20 seconds
+            override fun onTick(millisUntilFinished: Long) {
+                // Update UI or perform actions on each tick if needed
+            }
+
+            override fun onFinish() {
+                // Stop the Chronometer when the countdown timer finishes
+                viewTimer.stop()
+            }
+        }
+
+        buttonStart.setOnClickListener {
+            countdownTimer?.start()
             viewTimer.base = SystemClock.elapsedRealtime() + 20000
             viewTimer.start()
         }
