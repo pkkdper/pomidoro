b8daedebce56ac0da5e28911b296b705d2936396
oliwia <oliwia.giercza@gmail.com>
2023-09-16 10:18:11 +0100

    declared and initialized text view of timer

--- app/src/main/java/com/example/pomidoro/MainActivity.kt
+++ app/src/main/java/com/example/pomidoro/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.pomidoro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.TextView
 class MainActivity : AppCompatActivity() {
+    private lateinit var timer: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        timer = findViewById(R.id.text_view_timer)
     }
 }
