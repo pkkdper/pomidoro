2bab6500ac4530e6c138e5805277d8a33da15a79
oliwia <oliwia.giercza@gmail.com>
2023-09-16 10:23:47 +0100

    added a button - declared and initialized

--- app/src/main/java/com/example/pomidoro/MainActivity.kt
+++ app/src/main/java/com/example/pomidoro/MainActivity.kt
@@ -2,13 +2,16 @@ package com.example.pomidoro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 class MainActivity : AppCompatActivity() {
     private lateinit var timer: TextView
+    private lateinit var button_start: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         timer = findViewById(R.id.text_view_timer)
+        button_start = findViewById(R.id.button_start)
     }
 }
